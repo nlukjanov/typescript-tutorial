f270ccd726b5e820b13104a860f1e6631bdee55d
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-07-27 19:14:30 +0200

    changed file to ts;

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+const symbol = '#';
+const strLength = 7;
+function generateRandomId() {
+    return symbol + Math.random().toString(36).substr(2, strLength);
+}
+function main() {
+    const app = document.getElementById('app');
+    setInterval(() => {
+        app.innerHTML = generateRandomId();
+    }, 1000);
+}
+document.addEventListener('DOMContentLoaded', main);
