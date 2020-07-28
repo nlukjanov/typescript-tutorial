f6bcd0dbb9994b416cf2f8776ecc3cb20af40c7d
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-07-28 11:02:07 +0200

    add void and never functions + strict null checks

--- app.ts
+++ app.ts
@@ -4,10 +4,19 @@ function generateRandomId(symbol: ValidSymbol, strLength: number): string {
   return symbol + Math.random().toString(36).substr(2, strLength);
 }
 
+function userAlert(): void {
+  alert('Hello');
+}
+function userAlert2(): never {
+  throw new Error('Fail');
+}
+
 function main() {
   const app = document.getElementById('app');
   setInterval(() => {
-    app.innerHTML = generateRandomId('#', 7);
+    if (app) {
+      app.innerHTML = generateRandomId('#', 7);
+    }
   }, 1000);
 }
 
--- dist/app.js
+++ dist/app.js
@@ -2,10 +2,18 @@
 function generateRandomId(symbol, strLength) {
     return symbol + Math.random().toString(36).substr(2, strLength);
 }
+function userAlert() {
+    alert('Hello');
+}
+function userAlert2() {
+    throw new Error('Fail');
+}
 function main() {
     const app = document.getElementById('app');
     setInterval(() => {
-        app.innerHTML = generateRandomId('#', 7);
+        if (app) {
+            app.innerHTML = generateRandomId('#', 7);
+        }
     }, 1000);
 }
 document.addEventListener('DOMContentLoaded', main);
